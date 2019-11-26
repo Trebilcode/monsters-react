c28a9b170bc18685bc6c991c3a03ae6e3f53d2ca
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2019-11-26 18:30:30 -0500

    Updates

---
+++ precache-manifest.b16894bfa7bf07311b10fd7da3f6708e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d235f6d82543f38abd2eb21633364942",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "095e551f25342512f24b",
+    "url": "/monsters-react/static/css/main.e33b480e.chunk.css"
+  },
+  {
+    "revision": "71fb6a301d1d5702270e",
+    "url": "/monsters-react/static/js/2.8d2c28f8.chunk.js"
+  },
+  {
+    "revision": "095e551f25342512f24b",
+    "url": "/monsters-react/static/js/main.1eef918e.chunk.js"
+  },
+  {
+    "revision": "fc642772a43979096e7f",
+    "url": "/monsters-react/static/js/runtime-main.845ef866.js"
+  }
+]);
